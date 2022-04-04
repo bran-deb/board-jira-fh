c72be7f5d05d22f1bd97487681f89f954fb0f78f
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-03 23:35:21 -0400

    feat(Root): add states & check in forms

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,42 +1,78 @@
+import { ChangeEvent, useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
+
 import SaveIcon from '@mui/icons-material/SaveAsOutlined';
 import AddIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
+
 export const NewEntry = () => {
+
+    const [isAdding, setIsAdding] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [touched, setTouched] = useState(false);
+
+    const onTextFielChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value)
+    }
+
+    const onSave = () => {
+        if (inputValue.length === 0) return
+        setIsAdding(false)
+    }
+
+
     return (
         <Box sx={{ marginBottom: 2, padding: 1 }}>
-            <Button
-                fullWidth
-                variant='outlined'
-                startIcon={<AddIcon />}
-            >
-                Agregar Tarea
-            </Button>
+
+            {
+                (isAdding)
+                    ? (
+                        <>
+                            <TextField
+                                fullWidth
+                                sx={{ marginTop: 2, marginBottom: 1 }}
+                                placeholder='Nueva entrada'
+                                autoFocus
+                                multiline
+                                label='Nueva entrada'
+                                helperText={inputValue.length === 0 && touched && 'ingrese valor'}
+                                error={inputValue.length === 0 && touched}
+                                value={inputValue}
+                                onChange={onTextFielChanged}
+                                onBlur={() => setTouched(true)}     //pierde focus
+                            />
+
+                            <Box display={'flex'} justifyContent={'space-between'} margin={.5}>
+                                <Button onClick={() => { setIsAdding(false) }} variant="text">
+                                    Cancelar
+                                </Button>
+
+                                <Button
+                                    disabled={!inputValue}
+                                    onClick={onSave}
+                                    color="secondary"
+                                    endIcon={<SaveIcon />}
+                                    variant="outlined"
+                                >
+                                    Guardar
+                                </Button>
+                            </Box>
+                        </>
+                    )
+                    : (
+                        <Button
+                            fullWidth
+                            variant='outlined'
+                            startIcon={<AddIcon />}
+                            onClick={() => setIsAdding(true)}
+                        >
+                            Agregar Tarea
+                        </Button>
+                    )
+            }
 
 
-            <TextField
-                fullWidth
-                sx={{ marginTop: 2, marginBottom: 1 }}
-                placeholder='Nueva entrada'
-                autoFocus
-                multiline
-                label='Nueva entrada'
-                helperText='ingrese valor'
-            />
 
-            <Box display={'flex'} justifyContent={'space-between'} margin={.5}>
-                <Button variant="text">
-                    Cancelar
-                </Button>
-
-                <Button
-                    color="secondary"
-                    endIcon={<SaveIcon />}
-                    variant="outlined"
-                >
-                    Guardar
-                </Button>
-            </Box>
         </Box>
     )
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,12 +21,14 @@ const Home: NextPage = () => {
         <Grid item xs={12} sm={4}>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="Pendientes" />
+            <NewEntry />
             <EntryList status='in-progress' />
           </Card>
         </Grid>
         <Grid item xs={12} sm={4}>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="Pendientes" />
+            <NewEntry />
             <EntryList status='finished' />
           </Card>
         </Grid>
