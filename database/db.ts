6ef848cb43ebf32815b02ffb47bc3e90b03ce49e
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-06 18:22:19 -0400

    feat(Root): mongo database connection

---
+++ database/db.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+/**
+ * 0 disconnected
+ * 1 connected
+ * 2 connecting
+ * 3 disconnecting
+ */
+
+const mongooConection = {
+    isConnected: 0
+}
+
+export const connect = async () => {
+    if (mongooConection.isConnected) {
+        console.log('Ya estamos conectados');
+        return;
+    }
+    //si no esta conectado
+    if (mongoose.connections.length > 0) {
+        mongooConection.isConnected = mongoose.connections[0].readyState
+
+        if (mongooConection.isConnected === 1) {
+            console.log('Usando conexion anterior');
+            return;
+        }
+        await mongoose.disconnect()
+    }
+
+    await mongoose.connect('....')
+    mongooConection.isConnected = 1
+    console.log('Conectado a MongoDB', '....');
+}
+
+export const disconnect = async () => {
+
+    if (mongooConection.isConnected !== 0) return;
+
+    await mongoose.disconnect()
+    console.log('Desconectado de MongoDB');
+}
